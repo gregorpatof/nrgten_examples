43b6b7a0f9a10c765ad957f638b50514dde7120d
gregorpatof <shulgin@pop-os.localdomain>
2022-05-16 21:23:44 -0400

    troubleshooting

--- simple_test.py
+++ simple_test.py
@@ -6,8 +6,12 @@ except ImportError as e:
 	
 if __name__ == "__main__":
 	model = ENCoM("test_medium.pdb")
-	diff = abs(504.8468148072965 - model.compute_vib_entropy())
+	svib = model.compute_vib_entropy()
+	expected = 504.8468148072965
+	diff = abs(expected - svib)
 	if diff < 0.00001:
 		print("NRGTEN is properly installed on your system!")
 	else:
-		raise ValueError("NRGTEN seems installed on your system, but does not give the proper value for this test...")
+		raise ValueError("NRGTEN seems installed on your system, but does not give the proper value for this test. The"+
+						 " expected Svib value is {:.6f} and your installation produces {:.6f}.".format(expected,svib) +
+						 "If the difference is small enough, you can ignore this message.")
